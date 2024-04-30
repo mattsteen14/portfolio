09bddeabf2d20eb1e761c604b5b60fa16da31584
mattsteen14 <mattsteen14@me.com>
2024-04-30 11:33:51 +0100

    imported modules to js.

---
+++ js/script.mjs
@@ -0,0 +1,6 @@
+import { generateRandomNumber, immatureChoices } from "../../immatureGreeting/modules.mjs";
+import { immatureGreeting } from "../../immatureGreeting/script.mjs";
+
+console.log(immatureGreeting("male"));
+console.log(immatureGreeting("female"));
+console.log(immatureGreeting("neutral"));
