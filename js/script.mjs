0157a4bd831c0a9e736202ffe4a57cff4a61441f
Matt Steen-Brookes <mattsteen14@me.com>
2024-05-07 15:58:30 +0100

    Delete js directory

--- js/script.mjs
+++
@@ -1,6 +0,0 @@
-import { generateRandomNumber, immatureChoices } from "../../immatureGreeting/modules.mjs";
-import { immatureGreeting } from "../../immatureGreeting/script.mjs";
-
-console.log(immatureGreeting("male"));
-console.log(immatureGreeting("female"));
-console.log(immatureGreeting("neutral"));
